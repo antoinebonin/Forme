aaf847275fb4b9a995cf89e2e9cd9f02f4a2b886
Antoine Bonin <antoinebonin99@gmail.com>
2020-05-07 13:29:34 +0200

    Add email validator

--- Forme.php
+++ Forme.php
@@ -1,18 +1,28 @@
 <?php
 
+function checkEmail($field)
+{
+    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
 if (isset($_POST['email']))
 {
-    //TODO : Validation d'une adresse mail
-
-    //Envoi du mail
-    $to = "youremail@mail.fr";
-    $from = $_POST['email'];
-    $name = $_POST['name'];
-    $message = $_POST['message'];
-    //Présentation de l'email
-    $message = wordwrap($message, 70);
-    $headers = 'From: ' . $to . "\r\n" . 'Reply-To:' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-    mail($to, 'Nouveau message de : ' . $name ,$message ,$headers );
-
-    //TODO : Réponse au front
+    $check_email = checkEmail($_POST['email']);
+    if ($check_email == TRUE)
+    {
+        //Envoi du mail
+        $to = "youremail@mail.fr";
+        $from = $_POST['email'];
+        $name = $_POST['name'];
+        $message = $_POST['message'];
+        //Présentation de l'email
+        $message = wordwrap($message, 70);
+        $headers = 'From: ' . $to . "\r\n" . 'Reply-To:' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+        mail($to, 'Nouveau message de : ' . $name ,$message ,$headers );
+    }
 }
